d9fe6e2cb22642de76b2f957e1914f48652a1ec9
Domenic Belcastro <belcast1@msu.edu>
2025-09-09 12:18:03 -0400

    cleanup

--- android/src/main/java/com/twiliovoicereactnative/ExpoModule.kt
+++ android/src/main/java/com/twiliovoicereactnative/ExpoModule.kt
@@ -2,9 +2,6 @@ package com.twiliovoicereactnative
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import com.twilio.voice.ConnectOptions
-import java.util.UUID
-import com.twiliovoicereactnative.ReactNativeArgumentsSerializer.serializeCall
 
 class ExpoModule : Module() {
     private val log = SDKLog(this.javaClass)
@@ -12,30 +9,6 @@ class ExpoModule : Module() {
     override fun definition() = ModuleDefinition {
         Name("TwilioVoiceExpo")
 
-        Function("voice_connect") { accessToken: String ->
-            val context = appContext.reactContext ?: return@Function
-
-            val connectOptions = ConnectOptions.Builder(accessToken).build()
-            val uuid = UUID.randomUUID()
-            val callListenerProxy = CallListenerProxy(uuid, context)
-
-            val call = VoiceApplicationProxy.getVoiceServiceApi().connect(
-                connectOptions,
-                callListenerProxy
-            )
-
-            val callRecord = CallRecordDatabase.CallRecord(
-                uuid,
-                call,
-                "Callee", // provide a mechanism for determining the name of the callee
-                HashMap(), // provide a mechanism for passing custom TwiML parameters
-                CallRecordDatabase.CallRecord.Direction.OUTGOING,
-                "Display Name" // provide a mechanism for determining the notification display name of the callee
-            )
-
-            VoiceApplicationProxy.getCallRecordDatabase().add(callRecord)
-            
-            serializeCall(callRecord)
-        }
+        // For some reason I still need to have an empty module for things to work.
     }
 }
