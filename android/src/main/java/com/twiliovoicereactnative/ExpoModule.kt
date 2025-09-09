27d35dea2e4a2d0d82a6012fd98768939f9551b1
Domenic Belcastro <belcast1@msu.edu>
2025-09-09 12:06:43 -0400

    Revert "Delete module configuration"
    This reverts commit 962051497bcbcf250698959ae1b40c8e21f4d533.

---
+++ android/src/main/java/com/twiliovoicereactnative/ExpoModule.kt
@@ -0,0 +1,41 @@
+package com.twiliovoicereactnative
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+import com.twilio.voice.ConnectOptions
+import java.util.UUID
+import com.twiliovoicereactnative.ReactNativeArgumentsSerializer.serializeCall
+
+class ExpoModule : Module() {
+    private val log = SDKLog(this.javaClass)
+
+    override fun definition() = ModuleDefinition {
+        Name("TwilioVoiceExpo")
+
+        Function("voice_connect") { accessToken: String ->
+            val context = appContext.reactContext ?: return@Function
+
+            val connectOptions = ConnectOptions.Builder(accessToken).build()
+            val uuid = UUID.randomUUID()
+            val callListenerProxy = CallListenerProxy(uuid, context)
+
+            val call = VoiceApplicationProxy.getVoiceServiceApi().connect(
+                connectOptions,
+                callListenerProxy
+            )
+
+            val callRecord = CallRecordDatabase.CallRecord(
+                uuid,
+                call,
+                "Callee", // provide a mechanism for determining the name of the callee
+                HashMap(), // provide a mechanism for passing custom TwiML parameters
+                CallRecordDatabase.CallRecord.Direction.OUTGOING,
+                "Display Name" // provide a mechanism for determining the notification display name of the callee
+            )
+
+            VoiceApplicationProxy.getCallRecordDatabase().add(callRecord)
+            
+            serializeCall(callRecord)
+        }
+    }
+}
