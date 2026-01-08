d976c78898c48deda2f04cc98a8e05b00e6f03c8
Domenic Belcastro <belcast1@msu.edu>
2026-01-08 18:06:26 -0500

    quick patch
    https://github.com/twilio/twilio-voice-react-native/issues/604

--- android/src/main/java/com/twiliovoicereactnative/VoiceService.java
+++ android/src/main/java/com/twiliovoicereactnative/VoiceService.java
@@ -104,36 +104,75 @@ public class VoiceService extends Service {
     // against that case (GH-430).
     if (null != intent) {
       switch (Objects.requireNonNull(intent.getAction())) {
-        case ACTION_INCOMING_CALL:
-          incomingCall(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        case ACTION_INCOMING_CALL: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          incomingCall(callRecord);
           break;
-        case ACTION_ACCEPT_CALL:
+        }
+        case ACTION_ACCEPT_CALL: {
           try {
-            acceptCall(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+            final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+            if (null == callRecord) {
+              return START_NOT_STICKY;
+            }
+            acceptCall(callRecord);
           } catch (SecurityException e) {
             sendPermissionsError();
             logger.warning(e, "Cannot accept call, lacking necessary permissions");
           }
           break;
-        case ACTION_REJECT_CALL:
-          rejectCall(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        }
+        case ACTION_REJECT_CALL: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          rejectCall(callRecord);
           break;
-        case ACTION_CANCEL_CALL:
-          cancelCall(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        }
+        case ACTION_CANCEL_CALL: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          cancelCall(callRecord);
           break;
-        case ACTION_CALL_DISCONNECT:
-          disconnect(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        }
+        case ACTION_CALL_DISCONNECT: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          disconnect(callRecord);
           break;
-        case ACTION_RAISE_OUTGOING_CALL_NOTIFICATION:
-          raiseOutgoingCallNotification(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        }
+        case ACTION_RAISE_OUTGOING_CALL_NOTIFICATION: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          raiseOutgoingCallNotification(callRecord);
           break;
-        case ACTION_CANCEL_ACTIVE_CALL_NOTIFICATION:
-          cancelActiveCallNotification(getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        }
+        case ACTION_CANCEL_ACTIVE_CALL_NOTIFICATION: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          cancelActiveCallNotification(callRecord);
           break;
-        case ACTION_FOREGROUND_AND_DEPRIORITIZE_INCOMING_CALL_NOTIFICATION:
-          foregroundAndDeprioritizeIncomingCallNotification(
-            getCallRecord(Objects.requireNonNull(getMessageUUID(intent))));
+        }
+        case ACTION_FOREGROUND_AND_DEPRIORITIZE_INCOMING_CALL_NOTIFICATION: {
+          final CallRecordDatabase.CallRecord callRecord = getCallRecord(Objects.requireNonNull(getMessageUUID(intent)));
+          if (null == callRecord) {
+            return START_NOT_STICKY;
+          }
+          foregroundAndDeprioritizeIncomingCallNotification(callRecord);
           break;
+        }
         case ACTION_PUSH_APP_TO_FOREGROUND:
           logger.warning("VoiceService received foreground request, ignoring");
           break;
@@ -387,7 +426,14 @@ public class VoiceService extends Service {
     return (UUID)intent.getSerializableExtra(Constants.MSG_KEY_UUID);
   }
   private static CallRecordDatabase.CallRecord getCallRecord(final UUID uuid) {
-    return Objects.requireNonNull(getCallRecordDatabase().get(new CallRecordDatabase.CallRecord(uuid)));
+    //return Objects.requireNonNull(getCallRecordDatabase().get(new CallRecordDatabase.CallRecord(uuid)));
+    final CallRecordDatabase.CallRecord callRecord = getCallRecordDatabase().get(new CallRecordDatabase.CallRecord(uuid));
+    if (null == callRecord) {
+      logger.warning("No call record found");
+      sendCallDoesNotExistError();
+      return null;
+    }
+    return callRecord;
   }
   private static void sendJSEvent(@NonNull String scope, @NonNull WritableMap event) {
     getJSEventEmitter().sendEvent(scope, event);
@@ -395,6 +441,15 @@ public class VoiceService extends Service {
   private static void sendPermissionsError() {
     final String errorMessage = "Missing permissions.";
     final int errorCode = 31401;
+    getJSEventEmitter().sendEvent(ScopeVoice, constructJSMap(
+      new Pair<>(VoiceEventType, VoiceEventError),
+      new Pair<>(VoiceErrorKeyError, serializeError(errorCode, errorMessage))
+    ));
+  }
+
+  private static void sendCallDoesNotExistError() {
+    final String errorMessage = "Call does not exist.";
+    final int errorCode = 31481;
     getJSEventEmitter().sendEvent(ScopeVoice, constructJSMap(
       new Pair<>(VoiceEventType, VoiceEventError),
       new Pair<>(VoiceErrorKeyError, serializeError(errorCode, errorMessage))
